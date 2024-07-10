f1dfc4c894e545aaf497f613d18ab98e7778fb32
Ankita001812 <132539778+Ankita001812@users.noreply.github.com>
2024-07-11 03:02:57 +1000

    got initial things

---
+++ greenie/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+// import orangenavel from "../images/orangenavel.jpg";
+// import kiwi from "../images/organic-kiwis.jpg";
+
+export default function Home() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
--- greenie/src/pages/Nav.js
+++ greenie/src/pages/Nav.js
@@ -1,9 +1,57 @@
-import React from 'react'
+import React from "react";
+import bg from "../images/bg.png";
+import "./Nav.css";
+import { Link } from "react-router-dom";
 
-export default function Nav() {
+function Nav() {
   return (
     <div>
-      
+      <img src={bg} className="bg-img" alt="background-image" />
+      <nav className="navbar navbar-expand-lg navbar-dark">
+        <div className="container">
+          <div className="navbar-brand ">
+            {/* <Link to="/" className="nav-link"> */}
+              <span className="fw-bold text-light display-4">GrEEniE</span>
+            {/* </Link> */}
+          </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse display-6" id="navbarSupportedContent">
+            <ul className="navbar-nav ms-auto fw-bold">
+              <li className="nav-item active">
+                <Link to="/" className="nav-link">
+                  Home
+                </Link>
+              </li>
+              <li className="nav-item ">
+                <Link to="/vegetables" className="nav-link">
+                  Vegetables
+                </Link>
+              </li>
+              <li className="nav-item ">
+                <Link to="/fruits" className="nav-link">
+                  Fruits
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </div>
-  )
+  );
 }
+
+
+
+
+
+export default Nav;
