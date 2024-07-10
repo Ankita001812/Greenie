69e8d1ce3b55c286f3c7df9aa9eaf76d8bb7a006
Ankita001812 <132539778+Ankita001812@users.noreply.github.com>
2024-07-11 04:32:40 +1000

    experiments

--- greenie/src/pages/Fru.js
+++ greenie/src/pages/Fru.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Fru = () => {
   return (
-    <div className="veg-container">
-      <h1 className="display-2">fruits</h1>
+    <div className="container">
+      <h1 className="display-4 text-light">fruits</h1>
+      <h1 className="display-2 ">fruits</h1>
     </div>
   );
 };
