f8dd7a43f2ee7fe8175fe5fd84d75c8e385dfa4d
Ankita001812 <132539778+Ankita001812@users.noreply.github.com>
2024-07-14 01:10:27 +1000

    veg done

--- greenie/src/pages/Fru.js
+++ greenie/src/pages/Fru.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 const Fru = () => {
   return (
     <div className="container">
--- greenie/src/pages/Veg.js
+++ greenie/src/pages/Veg.js
@@ -1,10 +1,144 @@
-import React from 'react';
+import React from "react";
 import "./Veg.css";
 
+import avocado from "../images/avocado.png";
+import brocoli from "../images/brocoli.jpg";
+import brownonion from "../images/brownonion.png";
+import redonion from "../images/redonion.png";
+import cherrytomato from "../images/cherrytomato.jpg";
+import garlic from "../images/garlic.jpg";
+import greenchilli from "../images/greenchilli.jpg";
+import potato from "../images/potato.png";
+import redcapsicum from "../images/redcapsicum.jpg";
+import carrots from "../images/organic-carrots.jpg";
+import tomatoes from "../images/organic-tomatoes.jpg";
+import cabbage from "../images/cabbage.jpg";
+
 const Veg = () => {
+  const vegies = [
+    {
+      name: "Avocado Shepard",
+      price: 1.0,
+      per: "1EA",
+      quantity: 0,
+      img: avocado,
+      rate: 0,
+    },
+    {
+      name: "Fresh Broccoli",
+      price: 2.15,
+      per: "1EA",
+      quantity: 0,
+      img: brocoli,
+      rate: 0,
+    },
+    {
+      name: "Brown Onion",
+      price: 2.45,
+      per: "kg",
+      quantity: 0,
+      img: brownonion,
+      rate: 0,
+    },
+
+    {
+      name: "Cherry Tomatoes",
+      price: 0.26,
+      per: "1EA",
+      quantity: 0,
+      img: cherrytomato,
+      rate: 0,
+    },
+
+    {
+      name: "Green Chilli",
+      price: 7.6,
+      per: "1OOG",
+      quantity: 0,
+      img: greenchilli,
+      rate: 0,
+    },
+
+    {
+      name: "Garlic Head",
+      price: 1.55,
+      per: "1EA",
+      quantity: 0,
+      img: garlic,
+      rate: 0,
+    },
+    {
+      name: "Potato Brushed",
+      price: 0.73,
+      per: "1EA",
+      quantity: 0,
+      img: potato,
+      rate: 0,
+    },
+    {
+      name: "Red Capsicum",
+      price: 1.98,
+      per: "1EA",
+      quantity: 0,
+      img: redcapsicum,
+    },
+    {
+      name: "Red Onion",
+      price: 9.85,
+      per: "kg",
+      quantity: 0,
+      img: redonion,
+      rate: 0,
+    },
+    {
+      name: "Organic Carrots",
+      price: 2.99,
+      per: "kg",
+      quantity: 0,
+      img: carrots,
+      rate: 0,
+    },
+    {
+      name: "Organic Tomatoes",
+      price: 4.99,
+      per: "kg",
+      quantity: 0,
+      img: tomatoes,
+      rate: 0,
+    },
+    {
+      name: "Cabbage Savoy Half Each",
+      price: 4.99,
+      per: "1EA",
+      quantity: 0,
+      img: cabbage,
+      rate: 0,
+    },
+  ];
+
+  // const [veg, setVeg] = useState(vegies);
+
   return (
-    <div className="veg-container">
-      <h1 className="display-2">Vegetables</h1>
+    <div className="container">
+      <h1 className="display-5 fw-bold text-center my-4">Vegetables</h1>
+
+      <div className="row">
+        
+          {vegies.map((v, i) => (
+            <div className="col-12 col-sm-6 col-md-4 col-lg-3 mb-4" key={i}>
+              <div className="card h-100">
+                <img src={v.img} className="card-img-top" alt={v.name} />
+                <div className="card-body">
+                  <h5 className="card-title">{v.name}</h5>
+                  <p className="card-text">
+                    <strong>${v.price}</strong>/{v.per}
+                  </p>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      
     </div>
   );
 };
